d1dbdd5b6c7de4b224069ac4b810ac03c9b8b8fc
cpx2019 <1028598538@qq.com>
2020-01-05 14:46:57 +0800

    用户注册、登录、登出、校验用户名/邮箱、忘记ç±å密码-获取问题&校验答案、获取当前登录用户信息

---
+++ src/main/java/com/mmall/service/impl/IUserServiceImpl.java
@@ -0,0 +1,106 @@
+package com.mmall.service.impl;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.common.TokenCache;
+import com.mmall.dao.UserMapper;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import com.mmall.util.MD5Util;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service("iUserService")
+public class IUserServiceImpl implements IUserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+        ServerResponse<String> vaildResponse = checkVaild(username, Const.USERNAME);
+        if(vaildResponse.isSuccess()) {
+            return ServerResponse.createByErrorMessage("用户名不存在");
+        }
+
+        String md5Password = MD5Util.MD5EncodeUtf8(password);
+        User user = userMapper.selectLogin(username, md5Password);
+        if(user == null) {
+            return ServerResponse.createByErrorMessage("密码错误");
+        }
+
+        return ServerResponse.createBySuccess("登录成功", user);
+    }
+
+    @Override
+    public ServerResponse<String> register(User user) {
+        ServerResponse<String> vaildResponse = checkVaild(user.getUsername(), Const.USERNAME);
+        if(!vaildResponse.isSuccess()) {
+            return vaildResponse;
+        }
+
+        vaildResponse = checkVaild(user.getEmail(), Const.EMAIL);
+        if(!vaildResponse.isSuccess()) {
+            return vaildResponse;
+        }
+
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        //MD5加密
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+        int resultCount = userMapper.insert(user);
+        if(resultCount == 0) {
+            return ServerResponse.createByErrorMessage("注册失败");
+        }
+        return ServerResponse.createBySuccessMessage("注册成功");
+    }
+
+    @Override
+    public ServerResponse<String> checkVaild(String str, String type) {
+        if(StringUtils.isNotBlank(type)) {
+            if(Const.USERNAME.equals(type)) {
+                int resultCount = userMapper.checkUsername(str);
+                if(resultCount > 0) {
+                    return ServerResponse.createByErrorMessage("用户名已存在");
+                }
+            }
+
+            if(Const.EMAIL.equals(type)) {
+                int resultCount = userMapper.checkEmail(str);
+                if(resultCount > 0) {
+                    return ServerResponse.createByErrorMessage("邮箱已存在");
+                }
+            }
+        } else {
+            return ServerResponse.createByErrorMessage("参数错误");
+        }
+        return ServerResponse.createBySuccessMessage("校验成功");
+    }
+
+    @Override
+    public ServerResponse<String> selectQuestion(String username) {
+        ServerResponse<String> vaildResponse = checkVaild(username, Const.USERNAME);
+        if(vaildResponse.isSuccess()) {
+            return ServerResponse.createByErrorMessage("用户不存在");
+        }
+        String question = userMapper.selectQuestion(username);
+        if(StringUtils.isNotBlank(question)) {
+            return ServerResponse.createBySuccess(question);
+        }
+        return ServerResponse.createByErrorMessage("找回密码的问题是空的");
+    }
+
+    @Override
+    public ServerResponse<String> checkAnswer(String username, String question, String answer) {
+        int resultcount = userMapper.checkAnswer(username, question, answer);
+        if(resultcount > 0) {
+            String forgetToken = UUID.randomUUID().toString();
+            TokenCache.setKey(TokenCache.TOKEN_PREFIX+username,forgetToken);
+            return ServerResponse.createBySuccess(forgetToken);
+        }
+        return ServerResponse.createByErrorMessage("问题答案错误");
+    }
+
+}
